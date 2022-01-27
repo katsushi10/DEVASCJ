cb825e8e74a2af677bb44634aa131447ca3f201b
Top Out Human Capital Inc <89717403+TopOutHC@users.noreply.github.com>
2022-01-27 20:28:41 +0900

    Update save_config.py

--- netconf/save_config.py
+++ netconf/save_config.py
@@ -46,14 +46,14 @@ save_body = """
 <cisco-ia:save-config xmlns:cisco-ia="http://cisco.com/yang/cisco-ia"/>
 """
 
-print("Opening NETCONF Connection to {}".format(env_lab.IOS_XE_1["host"]))
+print("Opening NETCONF Connection to {}".format(env_lab.DEVASC["host"]))
 
 # Open a connection to the network device using ncclient
 with manager.connect(
-        host=env_lab.IOS_XE_1["host"],
-        port=env_lab.IOS_XE_1["netconf_port"],
-        username=env_lab.IOS_XE_1["username"],
-        password=env_lab.IOS_XE_1["password"],
+        host=env_lab.DEVASC["host"],
+        port=env_lab.DEVASC["netconf_port"],
+        username=env_lab.DEVASC["username"],
+        password=env_lab.DEVASC["password"],
         hostkey_verify=False
         ) as m:
 
